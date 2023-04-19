fe0724b66d9e146a6c948bebd7e70524a51eee1f
Tan Jun Keat <tjkeat2002@gmail.com>
2023-04-19 22:00:57 +0800

    added singer client and updated singer entity

--- Client/SingerBooth.java
+++ Client/SingerBooth.java
@@ -1,4 +1,116 @@
 package Client;
+import Entity.Singer;
+import adt.MyHashMap;
+
+import java.util.Scanner;
 
 public class SingerBooth {
+    Scanner scanner = new Scanner(System.in);
+    private MyHashMap<String, Singer> singerMap;
+    MyHashMap<String, Singer> SingerList = new MyHashMap<String, Singer>();
+    public SingerBooth() {
+        singerMap = new MyHashMap<>();
+    }
+
+    public void initializeSinger() {
+        Singer SingerDetails1 = new Singer("001", "James", "010101-01-0101", "Male", "22", "<<Love Story>>");
+        SingerList.put(SingerDetails1.getId(), SingerDetails1);
+
+        Singer SingerDetails2 = new Singer("002", "Jimmy", "000601-01-0101", "Male", "23", "<<Counting Star>>");
+        SingerList.put(SingerDetails2.getId(), SingerDetails2);
+
+        Singer SingerDetails3 = new Singer("003", "Rebeca", "991230-01-0101", "Female", "24", "<<Unstoppable>>");
+        SingerList.put(SingerDetails3.getId(), SingerDetails3);
+
+        Singer SingerDetails4 = new Singer("004", "Alexandra", "020710-01-0101", "Female", "21", "<<Happier>>");
+        SingerList.put(SingerDetails4.getId(), SingerDetails4);
+
+        Singer SingerDetails5 = new Singer("005", "Albert", "970620-01-0101", "Male", "26", "<<Love Yourself>>");
+        SingerList.put(SingerDetails4.getId(), SingerDetails4);
+    }
+
+    public void searchSingerById(){
+        System.out.print("Enter Singer ID: ");
+        String singerId = scanner.nextLine();
+        Singer singer = SingerList.get(singerId);
+        if (singer != null) {
+            System.out.println("----Singer Details----");
+            System.out.println("ID: " + singer.getId());
+            System.out.println("Name: " + singer.getName());
+            System.out.println("IC: " + singer.getIcNo());
+            System.out.println("Gender: " + singer.getGender());
+            System.out.println("Age: " + singer.getAge());
+            System.out.println("Song Titles: " + singer.getSongTitles());
+        } else {
+            System.out.println("Singer with ID " + singerId + " not found.");
+        }
+    }
+
+    public void editSingerInfo() {
+        System.out.print("Enter singer ID: ");
+        String id = scanner.nextLine();
+
+        // Check if singer exists in HashMap
+        if (SingerList.containsKey(id)) {
+            Singer singer = SingerList.get(id);
+
+            // Display current singer information
+            System.out.println("Current singer information:");
+            System.out.println(singer);
+
+            // Prompt user for new singer information
+            System.out.print("Enter new name (press Enter to keep current name): ");
+            String name = scanner.nextLine();
+            if (!name.isEmpty()) {
+                singer.setName(name);
+            }
+
+            System.out.print("Enter new IC number (press Enter to keep current IC number): ");
+            String icNumber = scanner.nextLine();
+            if (!icNumber.isEmpty()) {
+                singer.setIcNo(icNumber);
+            }
+
+            System.out.print("Enter new gender (press Enter to keep current gender): ");
+            String gender = scanner.nextLine();
+            if (!gender.isEmpty()) {
+                singer.setGender(gender);
+            }
+
+            System.out.print("Enter new age (press Enter to keep current age): ");
+            String age = scanner.nextLine();
+            if (!age.isEmpty()) {
+                singer.setAge(age);
+            }
+
+            System.out.print("Enter new song title (press Enter to keep current song title): ");
+            String songTitle = scanner.nextLine();
+            if (!songTitle.isEmpty()) {
+                singer.setSongTitles(songTitle);
+            }
+
+            // Update singer information in HashMap
+            SingerList.put(singer.getId(), singer);
+
+            System.out.println("Singer information updated successfully.");
+        } else {
+            System.out.println("Singer not found.");
+        }
+    }
+
+//    public void voteForSinger() {
+//        System.out.print("Enter Singer ID: ");
+//        String singerId = scanner.nextLine();
+//        Singer singer = singerMap.get(singerId);
+//        if (singer != null) {
+//            int currentVoteCount = singer.getVoteCount();
+//            singer.setVoteCount(currentVoteCount + 1);
+//            System.out.println("Vote for Singer " + singerId + " recorded successfully.");
+//        } else {
+//            System.out.println("Singer ID " + singerId + " not found.");
+//        }
+//    }
+    //ON HOLD YAAA
 }
+
+
--- Entity/Singer.java
+++ Entity/Singer.java
@@ -1,5 +1,8 @@
 package Entity;
 
+import adt.DllNode;
+import adt.DoublyLinkListInterface;
+
 import java.util.Objects;
 
 /**
@@ -9,8 +12,9 @@ public class Singer implements Comparable<Singer>{
     // Data members
     private String id;
     private String name;
+    private String icNo;
     private String gender;
-    private int age;
+    private String age;
     private String songTitles;
     private static int voteCount = 0;
 
@@ -18,17 +22,19 @@ public class Singer implements Comparable<Singer>{
 
     }
     public Singer(String id) {
-        this(id, "", "", 0, "");
+        this(id, "","", "", "", "");
     }
 
-    public Singer(String id, String name, String gender, int age, String songTitles) {
+    public Singer(String id, String name, String icNo, String gender, String age, String songTitles) {
         this.id = id;
         this.name = name;
+        this.icNo = icNo;
         this.gender = gender;
         this.age = age;
         this.songTitles = songTitles;
         voteCount++;
     }
+
 
     // Getters
     public String getId() {
@@ -39,11 +45,15 @@ public class Singer implements Comparable<Singer>{
         return name;
     }
 
+    public String getIcNo() {
+        return icNo;
+    }
+
     public String getGender() {
         return gender;
     }
 
-    public int getAge() {
+    public String getAge() {
         return age;
     }
 
@@ -55,6 +65,7 @@ public class Singer implements Comparable<Singer>{
         return voteCount;
     }
 
+
     // Setters
     public void setId(String id) {
         this.id = id;
@@ -64,11 +75,15 @@ public class Singer implements Comparable<Singer>{
         this.name = name;
     }
 
+    public void setIcNo(String name) {
+        this.name = name;
+    }
+
     public void setGender(String gender) {
         this.gender = gender;
     }
 
-    public void setAge(int age) {
+    public void setAge(String age) {
         this.age = age;
     }
 
@@ -79,6 +94,7 @@ public class Singer implements Comparable<Singer>{
     public void setVoteCount(int voteCount) {
         this.voteCount = voteCount;
     }
+
 
     // Entity methods
     @Override
@@ -111,6 +127,6 @@ public class Singer implements Comparable<Singer>{
 
     @Override
     public int compareTo(Singer o) {
-        this.getVoteCount() > o.getVoteCount()
+        return 0;
     }
 }
