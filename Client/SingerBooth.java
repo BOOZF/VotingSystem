13f0ba07d8b9f5102ab5a9d56242537545c57cef
Tan Jun Keat <tjkeat2002@gmail.com>
2023-04-19 23:01:28 +0800

    Updated

--- Client/SingerBooth.java
+++ Client/SingerBooth.java
@@ -6,26 +6,22 @@ import java.util.Scanner;
 
 public class SingerBooth {
     Scanner scanner = new Scanner(System.in);
-    private MyHashMap<String, Singer> singerMap;
     MyHashMap<String, Singer> SingerList = new MyHashMap<String, Singer>();
-    public SingerBooth() {
-        singerMap = new MyHashMap<>();
-    }
 
     public void initializeSinger() {
-        Singer SingerDetails1 = new Singer("001", "James", "010101-01-0101", "Male", "22", "<<Love Story>>");
+        Singer SingerDetails1 = new Singer("001", "James", "abc001", "010101-01-0101", "Male", "22", "<<Love Story>>");
         SingerList.put(SingerDetails1.getId(), SingerDetails1);
 
-        Singer SingerDetails2 = new Singer("002", "Jimmy", "000601-01-0101", "Male", "23", "<<Counting Star>>");
+        Singer SingerDetails2 = new Singer("002", "Jimmy", "abc002", "000601-01-0101", "Male", "23", "<<Counting Star>>");
         SingerList.put(SingerDetails2.getId(), SingerDetails2);
 
-        Singer SingerDetails3 = new Singer("003", "Rebeca", "991230-01-0101", "Female", "24", "<<Unstoppable>>");
+        Singer SingerDetails3 = new Singer("003", "Rebeca", "abc003", "991230-01-0101", "Female", "24", "<<Unstoppable>>");
         SingerList.put(SingerDetails3.getId(), SingerDetails3);
 
-        Singer SingerDetails4 = new Singer("004", "Alexandra", "020710-01-0101", "Female", "21", "<<Happier>>");
+        Singer SingerDetails4 = new Singer("004", "Alexandra", "abc004", "020710-01-0101", "Female", "21", "<<Happier>>");
         SingerList.put(SingerDetails4.getId(), SingerDetails4);
 
-        Singer SingerDetails5 = new Singer("005", "Albert", "970620-01-0101", "Male", "26", "<<Love Yourself>>");
+        Singer SingerDetails5 = new Singer("005", "Albert", "abc005", "970620-01-0101", "Male", "26", "<<Love Yourself>>");
         SingerList.put(SingerDetails4.getId(), SingerDetails4);
     }
 
@@ -65,6 +61,12 @@ public class SingerBooth {
                 singer.setName(name);
             }
 
+            System.out.print("Enter new password (press Enter to keep current password): ");
+            String password = scanner.nextLine();
+            if (!password.isEmpty()) {
+                singer.setPassword(password);
+            }
+
             System.out.print("Enter new IC number (press Enter to keep current IC number): ");
             String icNumber = scanner.nextLine();
             if (!icNumber.isEmpty()) {
@@ -97,20 +99,6 @@ public class SingerBooth {
             System.out.println("Singer not found.");
         }
     }
-
-//    public void voteForSinger() {
-//        System.out.print("Enter Singer ID: ");
-//        String singerId = scanner.nextLine();
-//        Singer singer = singerMap.get(singerId);
-//        if (singer != null) {
-//            int currentVoteCount = singer.getVoteCount();
-//            singer.setVoteCount(currentVoteCount + 1);
-//            System.out.println("Vote for Singer " + singerId + " recorded successfully.");
-//        } else {
-//            System.out.println("Singer ID " + singerId + " not found.");
-//        }
-//    }
-    //ON HOLD YAAA
 }
 
 
